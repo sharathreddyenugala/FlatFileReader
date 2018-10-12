6833c39942e06d6e3ebf300be2d1c26a9756341b
Sharath <Mizpah_DEV@Mizpahsoft>
2018-10-12 18:37:56 +0530

    Validation with test cases

--- FlatFileReader3.0/Controllers/HomeController.cs
+++ FlatFileReader3.0/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FlatFileReader3._0.FileUploadClass;
+using FlatFileReader3._0.Db;
 
 namespace FlatFileReader3._0.Controllers
 {
@@ -82,7 +83,90 @@ namespace FlatFileReader3._0.Controllers
             return View();
         }
 
+        //This method will validate the starcite sheet as per test cases
+        public ActionResult validateStarCiteSheet()
+        {
 
-        
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult validateStarCiteSheet(HttpPostedFileBase FileUpload)
+        {
+
+            return View();
+        }
+
+
+        public string ValidateSheet()
+        {
+            string Result = String.Empty;
+            try
+            {
+                List<string> FailedArr = new List<string>();
+
+                DataTable dt = new DataTable();
+
+                StarciteDb StarciteDb = new StarciteDb();
+                var StarCiteConfig = StarciteDb.getStarCiteConfig();
+                dt = StarciteDb.getStarCiteTargetData("STARCITE_20180313_Global_target");
+
+                if (dt.Columns.Count == StarCiteConfig.Count) {
+                    if (dt.Rows.Count > 0) {
+
+                        for (int j = 0; j < dt.Rows.Count; j++) {
+                            if (StarCiteConfig.Count > 0)
+                            {
+                                for (int i = 0; i < StarCiteConfig.Count; i++)
+                                {
+                                    string fieldName = StarCiteConfig[i].fieldName;
+                                    int isRequired = Convert.ToInt32(StarCiteConfig[i].isRequired);
+                                    string actualValue = StarCiteConfig[i].actualValue;
+
+                                    string value = dt.Rows[j][fieldName].ToString();
+                                    if (isRequired == 1) {
+                                        if (value == "" || value == null)
+                                        {
+                                            int num = j + 1;
+                                            string failedText = "Row Number " + num + " " + fieldName + " failed, Value Required.";
+                                            FailedArr.Add(failedText);
+                                            break;
+                                        }
+                                    }
+                                    if (actualValue != "")
+                                    {
+                                        if (value != actualValue)
+                                        {
+                                            int num = j + 1;
+                                            string failedText = "Row Number " + num + " " + fieldName + " failed, Value incorrect. Given-" + value + ", Correct is " + actualValue;
+                                            FailedArr.Add(failedText);
+                                            break;
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                        Console.Write(FailedArr);
+                        if (FailedArr.Count == 0)
+                        {
+                            //Everything is fine.
+                            Result = "Success.";
+                        }
+                    }
+                }
+                else
+                {
+                    Result = "Configuration columns and Target columns are not matched.";
+                }
+
+            }catch(Exception ex)
+            {
+
+            }
+            return Result;
+        }
+
     }
 }
---
+++ FlatFileReader3.0/Db/StarciteDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using FlatFileReader3._0.Models;
+
+namespace FlatFileReader3._0.Db
+{
+    public class StarciteDb
+    {
+        MedispendEntities MedispendEntities = new MedispendEntities();
+        starcite_global_conf Starcite = new starcite_global_conf();
+
+        public virtual IList<starcite_global_conf> getStarCiteConfig()
+        {
+            var row = from starcite_global_conf in MedispendEntities.starcite_global_conf
+                      .Where(r => r.isActive == 1)
+                      select starcite_global_conf;
+            return row.ToList();
+
+        }
+
+
+        public DataTable getStarCiteTargetData(string tableName)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string cs = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+                string query = "select * from " + tableName;
+                SqlDataAdapter cmd = new SqlDataAdapter(query, cs);
+                cmd.Fill(dt);
+                   
+            }
+            catch(Exception ex)
+            {
+                
+            }
+
+            return dt;
+        }
+
+    }
+}
